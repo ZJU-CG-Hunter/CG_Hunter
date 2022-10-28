83aacb501094ea5c78d89ebf4397409b7a02a0fa
Markito_hui <107126477+MarkitoPlus@users.noreply.github.com>
2022-10-29 00:08:44 +0800

    第一次test

---
+++ CG_Hunter/test.cpp
@@ -0,0 +1,8 @@
+#include "test.h"
+
+
+int main() {
+	std::cout << "计算机图形学小组作业 " << std::endl;
+	std::cout << "小组成员：" << std::endl;
+	std::cout << member_1;
+}
---
+++ CG_Hunter/test.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+
+std::string member_1 = "»ÆË³êÍ";
